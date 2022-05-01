05d5164a3142de10983519099a45e802a15ce7b7
Daniel Rebolledo <danielrebolledo.182@gmail.com>
2022-05-01 14:53:19 -0400

    Primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1></h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>RUN</th>
+          <th>Nacimiento</th>
+          <th>Edad</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>{{usuario1.nombre}}</td>
+          <td>{{usuario1.apellido}}</td>
+          <td>{{usuario1.run}}</td>
+          <td>{{usuario1.nacimiento}}</td>
+          <td>{{usuario1.edad}}</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>{{usuario2.nombre}}</td>
+          <td>{{usuario2.apellido}}</td>
+          <td>{{usuario2.run}}</td>
+          <td>{{usuario2.nacimiento}}</td>
+          <td>{{usuario2.edad}}</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>{{usuario3.nombre}}</td>
+          <td>{{usuario3.apellido}}</td>
+          <td>{{usuario3.run}}</td>
+          <td>{{usuario3.nacimiento}}</td>
+          <td>{{usuario3.edad}}</td>
+        </tr>
+      </tbody>
+      
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: () =>({
+
+    usuario1 :{
+        nombre: "Jose",
+        apellido: "Perez",
+        run: "12345678-9",
+        nacimiento: "12-04-67",
+        edad: 52
+      },
+
+      usuario2 :{
+        nombre: "Maria",
+        apellido: "Rodriguez",
+        run: "12345678-9",
+        nacimiento: "90-11-90",
+        edad: 29
+      },
+
+      usuario3 :{
+        nombre: "Manuel",
+        apellido: "Patiño",
+        run: "14223456-6",
+        nacimiento: "02-12-59",
+        edad: 60
+      }
+  }),
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
